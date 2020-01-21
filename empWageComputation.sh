8db33ac62e224d3fa788485737ee92dcaaa4d496
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-21 17:39:15 +0530

    ADDED : Employee wage calculation for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,26 +1,34 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to employee wage"
 WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
-randomCheck=$((RANDOM%3))
+IS_ABSENT=0
+monthlyWage=0
 
 function WageCalculation() {
 	timeInHours=$1
-	echo "Daily Wage=$(($timeInHours * $WAGE_PER_HOUR))"
+	dailyWage=$(($timeInHours * $WAGE_PER_HOUR))
+	monthlyWage=$(($timeInHours * $WAGE_PER_HOUR + $monthlyWage))
+	echo "Daily Wage=$dailyWage"
+	echo "Monthly Wage=$monthlyWage"
 }
 
-case $randomCheck in
-	0)
-		echo "Employee is absent"
-		echo "Daily Wage=0"
-	;;
-	1)
-		echo "Employee is Full Time"
-		WageCalculation $FULL_TIME_HOUR
-	;;
-	2)
-		echo "Employee is Half Time"
-		WageCalculation $PART_TIME_HOUR
-	;;
-esac
+for((day=1; day<=20; day++))
+do
+	randomCheck=$((RANDOM%3))
+	case $randomCheck in
+		0)
+			echo "Employee is absent on day $day"
+			WageCalculation $IS_ABSENT
+		;;
+		1)
+			echo "Employee is Full Time on day $day"
+			WageCalculation $FULL_TIME_HOUR
+		;;
+		2)
+			echo "Employee is Half Time on day $day"
+			WageCalculation $PART_TIME_HOUR
+		;;
+	esac
+done
