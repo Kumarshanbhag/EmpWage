a66c0a9905d97af8e0ba2d3302ce479237db1e85
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-21 16:51:11 +0530

    ADDED : Part Time Employee Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -x
 echo "Welcome to employee wage"
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
-if(($((RANDOM%2))==0 ))
+FULL_TIME_HOUR=8
+PART_TIME_HOUR=4
+randomCheck=$((RANDOM%3))
+
+function WageCalculation() {
+	timeInHours=$1
+	echo "Daily Wage=$(($timeInHours * $WAGE_PER_HOUR))"
+}
+
+if [ $randomCheck -eq 0 ]
 then
 	echo "Employee is absent"
 	echo "Daily Wage=0"
+elif [ $randomCheck -eq 1 ]
+then
+	echo "Employee is Full Time"
+	WageCalculation $FULL_TIME_HOUR
 else
-	echo "Employee is present"
-	echo "Daily Wage=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))"
+	echo "Employee is Half Time"
+	WageCalculation $PART_TIME_HOUR
 fi
-#echo $dailyWage
