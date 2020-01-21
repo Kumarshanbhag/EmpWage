1628a84619e77d388336067d8cdd2139245f6135
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-21 19:11:23 +0530

    ADDED : Employee Wage Till 20Days or 80Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,34 +1,50 @@
-#!/bin/bash 
+#!/bin/bash -x
 echo "Welcome to employee wage"
 WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
 IS_ABSENT=0
 monthlyWage=0
+day=1
+workingHours=0
 
 function WageCalculation() {
 	timeInHours=$1
+	workingHours=$(($workingHours + $timeInHours))
 	dailyWage=$(($timeInHours * $WAGE_PER_HOUR))
 	monthlyWage=$(($timeInHours * $WAGE_PER_HOUR + $monthlyWage))
 	echo "Daily Wage=$dailyWage"
 	echo "Monthly Wage=$monthlyWage"
+	echo "Working Hours=$workingHours"
 }
 
-for((day=1; day<=20; day++))
-do
-	randomCheck=$((RANDOM%3))
-	case $randomCheck in
+function EmployeeAttendance() {
+randomCheck=$1
+case $randomCheck in
 		0)
 			echo "Employee is absent on day $day"
 			WageCalculation $IS_ABSENT
 		;;
 		1)
 			echo "Employee is Full Time on day $day"
-			WageCalculation $FULL_TIME_HOUR
+			WageCalculation $PART_TIME_HOUR
 		;;
 		2)
 			echo "Employee is Half Time on day $day"
-			WageCalculation $PART_TIME_HOUR
+			WageCalculation $FULL_TIME_HOUR
 		;;
 	esac
+}
+
+while [[ $day -le 20 && $workingHours -lt 100 ]]
+do
+	if(($workingHours > 92))
+	then
+		randomCheck=$((RANDOM%2))
+		EmployeeAttendance $randomCheck
+	else
+		randomCheck=$((RANDOM%3))
+		EmployeeAttendance $randomCheck
+	fi
+	((day++))
 done
