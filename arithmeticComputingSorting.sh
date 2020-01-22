8f7376216c09f70d761eb8d79d2a635b98f1fbb4
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-22 18:09:47 +0530

    ADDED : Accepted 3 Values From User

--- arithmeticComputingSorting.sh
+++ arithmeticComputingSorting.sh
@@ -1,3 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Arithmetic Computing and Sorting"
+read -p "Enter 3 values to perform operation " a b c
+echo -e "Three numbers are : \n$a \n$b \n$c"
 
