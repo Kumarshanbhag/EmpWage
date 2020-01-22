4ec5e49e147c17b94513467c0d48e34347d50b3c
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-22 11:32:11 +0530

    ADDED : Daily Wage Changed To Array

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,42 +9,43 @@ day=1
 working_Hours=0
 
 function WageCalculation() {
-	timeInHours=$1
-	working_Hours=$(($working_Hours + $timeInHours))
-	dailyWage=$(($timeInHours * $WAGE_PER_HOUR))
-	monthlyWage=$(($timeInHours * $WAGE_PER_HOUR + $monthlyWage))
-	echo "Daily Wage=$dailyWage"
-	echo "Monthly Wage=$monthlyWage"
-	echo "Working Hours=$working_Hours"
+   timeInHours=$1
+   day=$2
+   working_Hours=$(($working_Hours + $timeInHours))
+   dailyWage[$day]=$(($timeInHours * $WAGE_PER_HOUR))
+   monthlyWage=$((${dailyWage[$day]} + $monthlyWage))
+   echo "Working Hours=$working_Hours"
 }
 
 function EmployeeAttendance() {
 randomCheck=$1
 case $randomCheck in
-		0)
-			echo "Employee is absent on day $day"
-			WageCalculation $IS_ABSENT
-		;;
-		1)
-			echo "Employee is Full Time on day $day"
-			WageCalculation $PART_TIME_HOUR
-		;;
-		2)
-			echo "Employee is Half Time on day $day"
-			WageCalculation $FULL_TIME_HOUR
-		;;
-	esac
+      0)
+         echo "Employee is absent on day $day"
+         WageCalculation $IS_ABSENT $day
+      ;;
+      1)
+         echo "Employee is Part Time on day $day"
+         WageCalculation $PART_TIME_HOUR $day 
+      ;;
+      2)
+         echo "Employee is Full Time on day $day"
+         WageCalculation $FULL_TIME_HOUR $day
+      ;;
+   esac
 }
 
 while [[ $day -le 20 && $working_Hours -lt 100 ]]
 do
-	if(($workingHours > 92))
-	then
-		randomCheck=$((RANDOM%2))
-		EmployeeAttendance $randomCheck
-	else
-		randomCheck=$((RANDOM%3))
-		EmployeeAttendance $randomCheck
-	fi
-	((day++))
+   if(($working_Hours > 92))
+   then
+      randomCheck=$((RANDOM%2))
+      EmployeeAttendance $randomCheck
+   else
+      randomCheck=$((RANDOM%3))
+      EmployeeAttendance $randomCheck
+   fi
+   echo "Daily Wage=${dailyWage[$day]}"
+   ((day++))
 done
+echo "Monthly Wage=$monthlyWage"
