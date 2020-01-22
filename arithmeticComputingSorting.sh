8556cd620ae40d55566a47279d914298406df04e
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-22 19:07:01 +0530

    ADDED : Operation Values Stored In Dictionary

--- arithmeticComputingSorting.sh
+++ arithmeticComputingSorting.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
+declare -A result
 echo "Welcome to Arithmetic Computing and Sorting"
 read -p "Enter 3 values to perform operation " a b c
 echo -e "Three numbers are : \n$a \n$b \n$c"
-f1=$(($a+$b*$c))
-f2=$(($a*$b+$c))
-f3=$(($c+$a/$b))
-f4=$(($a%$b+$c))
-echo "$a + $b * $c = $f1"
-echo "$a * $b + $c = $f2"
-echo "$c + $a / $b = $f3"
-echo "$a % $b + $c = $f4"
+result[Operation1]=$(($a+$b*$c))
+result[Operation2]=$(($a*$b+$c))
+result[Operation3]=$(($c+$a/$b))
+result[Operation4]=$(($a%$b+$c))
+echo "$a + $b * $c = ${result[Operation1]}"
+echo "$a * $b + $c = ${result[Operation2]}"
+echo "$c + $a / $b = ${result[Operation3]}"
+echo "$a % $b + $c = ${result[Operation4]}"
