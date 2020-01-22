f4ba1c1020ddc475322ca06b4ea1656742cdde8a
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-22 18:54:37 +0530

    ADDED : Fourth Operation Completed

--- arithmeticComputingSorting.sh
+++ arithmeticComputingSorting.sh
@@ -5,6 +5,8 @@ echo -e "Three numbers are : \n$a \n$b \n$c"
 f1=$(($a+$b*$c))
 f2=$(($a*$b+$c))
 f3=$(($c+$a/$b))
+f4=$(($a%$b+$c))
 echo "$a + $b * $c = $f1"
 echo "$a * $b + $c = $f2"
 echo "$c + $a / $b = $f3"
+echo "$a % $b + $c = $f4"
