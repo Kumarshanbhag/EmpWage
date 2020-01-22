6d6a62dea01667ff44ae4815f27fbd62fd514353
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-22 18:38:49 +0530

    ADDED : Second Operation Completed

--- arithmeticComputingSorting.sh
+++ arithmeticComputingSorting.sh
@@ -3,4 +3,6 @@ echo "Welcome to Arithmetic Computing and Sorting"
 read -p "Enter 3 values to perform operation " a b c
 echo -e "Three numbers are : \n$a \n$b \n$c"
 f1=$(($a+$b*$c))
+f2=$(($a*$b+$c))
 echo "$a + $b * $c = $f1"
+echo "$a * $b + $c = $f2"
