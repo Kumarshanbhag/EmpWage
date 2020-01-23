7fa24d8e87e82ccc887b2c5d1399fea9eb5489c7
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-23 10:17:26 +0530

    ADDED : Result Stored In Array From Dictionary

--- arithmeticComputingSorting.sh
+++ arithmeticComputingSorting.sh
@@ -3,11 +3,19 @@ declare -A result
 echo "Welcome to Arithmetic Computing and Sorting"
 read -p "Enter 3 values to perform operation " a b c
 echo -e "Three numbers are : \n$a \n$b \n$c"
-result[Operation1]=$(($a+$b*$c))
-result[Operation2]=$(($a*$b+$c))
-result[Operation3]=$(($c+$a/$b))
-result[Operation4]=$(($a%$b+$c))
-echo "$a + $b * $c = ${result[Operation1]}"
-echo "$a * $b + $c = ${result[Operation2]}"
-echo "$c + $a / $b = ${result[Operation3]}"
-echo "$a % $b + $c = ${result[Operation4]}"
+count=1
+result["Operation "$((count++))]=$(($a+$b*$c))
+result["Operation "$((count++))]=$(($a*$b+$c))
+result["Operation "$((count++))]=$(($c+$a/$b))
+result["Operation "$((count++))]=$(($a%$b+$c))
+
+for((i=1; i<=4; i++))
+do
+	arr[$i]=${result[Operation $i]}
+done
+
+echo "$a + $b * $c = ${arr[1]}"
+echo "$a * $b + $c = ${arr[2]}"
+echo "$c + $a / $b = ${arr[3]}"
+echo "$a % $b + $c = ${arr[4]}"
+
