b62e6b983758ccba9eb5b931b30c5817a89be385
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-27 15:07:34 +0530

    ADDED : Gamble Till 50% Stake Won Or Lost

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,15 +1,28 @@
 #!/bin/bash -x
 echo "Welcome to Gambling Simulation"
+function Gamble(){
+while [[ $cash -lt $(($STAKE+$percent)) && $cash -gt $(($STAKE-$percent)) ]]
+do
+	if((RANDOM%2==0))
+	then
+		echo "Bet Won"
+		((cash++))
+	else
+		echo "Bet Lost"
+		((cash--))
+	fi
+	echo "Stake=$cash"
+done
+}
+
+function PercentageLimit(){
+percent=$(($STAKE*50/100));
+Gamble
+}
+
 STAKE=100
 BET=1
 cash=$STAKE
+PercentageLimit
 
-if((RANDOM%2==0))
-then
-	echo "Bet Won"
-	((cash++))
-else
-	echo "Bet Lost"
-	((cash--))
-fi
 
